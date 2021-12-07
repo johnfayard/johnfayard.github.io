3d509bdd8fa2eb6b004d2e26beab418c19b55cd7
johnfayard <johnfayard49@gmail.com>
2021-12-07 14:46:03 +0000

    changed color once again

--- projects/platformer/js/factory/platform.js
+++ projects/platformer/js/factory/platform.js
@@ -9,7 +9,7 @@
         game.platforms.enableBody = true;
         
         function create(x, y, scaleX, scaleY, immovable) {
-            var platform = game.platforms.create(x, y, 'red');
+            var platform = game.platforms.create(x, y, 'platform');
             platform.scale.setTo(scaleX || 1, scaleY || 1);
             platform.body.immovable = immovable || true;
             return platform;
